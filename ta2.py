f88db0ea786ce81f30ad41b3cbf4466073b7e713
Tw1tchC <113410216+Tw1tchC@users.noreply.github.com>
2024-09-17 12:58:45 +0200

    Add files via upload

---
+++ ta2.py
@@ -0,0 +1,6 @@
+tablica = [-9,9,8,-7,6,-5,4,-3,2,-1]
+index = 1
+N = 10
+while (index <= N):
+    print(tablica[index])
+    index += 1
---
+++ tab.py
@@ -0,0 +1,9 @@
+tablica = [-9,9,8,-7,6,-5,4,-3,2,-1]
+index = 1
+N = 10
+while True:
+    if index <= N:
+        print(tablica[index])
+        index += 1
+    else:
+        break    
